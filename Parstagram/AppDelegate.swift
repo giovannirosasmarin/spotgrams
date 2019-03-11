c71cce1cdc0a5cd72daf7d46424f3235f70cd9ad
Giovanni Rosas-Marin <marin@Giovannis-MacBook-Pro.local>
2019-03-11 00:10:29 -0700

    some what done :(

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Pastragram"
+                configuration.server = "https://agile-journey-72937.herokuapp.com/parse"
+            })
+        )
+        
+        
         return true
     }
 
