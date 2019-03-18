f8946e1a24e66ae9068d07172a9ec6e979a2deaf
Giovanni Rosas-Marin <marin@Giovannis-MacBook-Pro.local>
2019-03-18 00:31:16 -0700

    hola

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,26 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Giovanni Rosas-Marin on 3/17/19.
+//  Copyright © 2019 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
